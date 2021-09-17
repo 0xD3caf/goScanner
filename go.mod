6d822761b2c5c6aae8b89f20326649d4bdef049b
0xD3caf <66028416+0xD3caf@users.noreply.github.com>
2021-09-16 22:21:43 -0500

    updated UDP scanning
    UDP scanning totally reworked. Added support for repeated checks per port and basic logic for checking if port is open. Also a small function to try and return specific ICMP packets for checking functions.

--- go.mod
+++ go.mod
@@ -4,7 +4,4 @@ go 1.17
 
 require golang.org/x/net v0.0.0-20210913180222-943fd674d43e
 
-require (
-	github.com/google/gopacket v1.1.19 // indirect
-	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
-)
+require golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
--- main.go
+++ main.go
@@ -10,10 +10,16 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/pcap"
 	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
+
+/************************************************
+*                                               *
+*       UDP SCANNING REQUIRES ROOT PRIV         *
+*                                               *
+*************************************************
+
 
 //Basic port scanner built in Go
 // takes flags to indicate information
@@ -23,6 +29,7 @@ import (
 // -proto (Sets either TCP or UDP for proto, default tcp
 // -timeout (sets the amount of time to wait for connections in seconds)
 // -display (sets whether to display closed ports, default off, flag/ does not need input)
+// -retry (sets number of attempts per port for UDP scanning)
 
 // scanner.go -ip <Target_IP> -port <port | Min-Max> -proto {tcp | udp} -timeout {seconds} -display
 
@@ -33,32 +40,31 @@ import (
 //add real UDP scanning
 // -outfile (optional dump to file)
 //-help (helpfile)
+//ABILITY TO RANDOMIZE SOURCE PORT
 
 //Update UDP scanning, again. Might try libpcap to collect ICMP response
 //first step is to capture controlled pings
-func main() {
-	//starting up and getting flag values
 
-	if handle, err := pcap.OpenLive("wlan", 1600, true, pcap.BlockForever); err != nil {
-		panic(err)
-	} else if err := handle.SetBPFFilter("tcp and port 80"); err != nil { // optional
-		panic(err)
-	} else {
-		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-		for packet := range packetSource.Packets() {
-			fmt.Println(packet.String())
-		}
-	}
+*/
+func main() {
+
+	//LOCAL ADDRESS TO SCAN FROM (HOST:PORT)
+	local_addr := "10.13.37.6:45555"
+
+	//starting up and getting flag values
 	IPPtr := flag.String("ip", "Localhost", "IP Selection")
 	portPtr := flag.String("port", "0", "Port or port range to scan")
 	protoPtr := flag.String("proto", "tcp", "Select either UDP or TCP scanning")
 	timeoutPtr := flag.String("timeout", "10", "Set amount of time before timing out connection")
 	displayPtr := flag.Bool("display", false, "Sets whether to display closed ports")
+	retryPtr := flag.String("retry", "5", "retry attempts for UDP scanning")
 
 	flag.Parse()
+	if !isValidIP(net.ParseIP(*IPPtr)) { //check if IP is valud
+		fmt.Println("This IP is not Valid, Please try again")
+		os.Exit(3)
+	}
 	timeout, _ := strconv.Atoi(*timeoutPtr)
-	local_addr := "10.13.37.5:0"
-	curr_addr := "10.13.37.5"
 	var port int
 
 	FinalPortList := make([]int, 0)
@@ -78,11 +84,8 @@ func main() {
 		fmt.Printf("Proto is: %s\n", *protoPtr)
 		fmt.Printf("Timeout is: %s\n", timeout)
 	*/
-	proto := strings.ToLower(*protoPtr)  //convert to lower case to match conn input
-	if !isValidIP(net.ParseIP(*IPPtr)) { //check if IP is valud
-		fmt.Println("This IP is not Valid, Please try again")
-		os.Exit(3)
-	}
+	proto := strings.ToLower(*protoPtr) //convert to lower case to match conn input
+
 	//needs check for every object in range
 	for i := 0; i < len(FinalPortList); i++ { //loop through all ports checking if valid
 		if !isValidPort(FinalPortList[i]) {
@@ -118,41 +121,76 @@ func main() {
 			//send several times, add flag for number of repeats
 
 			//set up icmp listener
-			_ = curr_addr
-			icmp_conn, icmp_err := icmp.ListenPacket("ip4:icmp", "127.0.0.1")
+			ip_str := strings.Split(local_addr, ":")[0]
+			icmp_conn, icmp_err := icmp.ListenPacket("ip4:icmp", ip_str)
 			if icmp_err != nil {
 				fmt.Println("Houston, we have an error")
+				fmt.Println(icmp_err)
 			}
 			var msg []byte
 
 			//start UDP setup
-			udpAddr, _ := net.ResolveUDPAddr("udp4", ip_string)
-			laddr, _ := net.ResolveUDPAddr("udp4", local_addr)
-			conn, _ := net.DialUDP("udp", laddr, udpAddr)
+			udpAddr, err := net.ResolveUDPAddr("udp4", ip_string)
+			handle_err(err)
+			laddr, err := net.ResolveUDPAddr("udp4", local_addr)
+			handle_err(err)
+			conn, err := net.DialUDP("udp", laddr, udpAddr)
+			handle_err(err)
+			icmp_conn.SetDeadline(time.Now().Add(1 * time.Second)) //sets wait time
 
 			deadline := time.Now().Add(time.Duration(timeout)) //computes timeout time and sets on conn
-			conn.SetDeadline(deadline)
+			err = conn.SetDeadline(deadline)
+			handle_err(err)
 			buffer := []byte("Hello")
-			_, err := conn.Write(buffer) //send buffered data
-			fmt.Println(err)
+			max := *retryPtr
+			int_max, _ := strconv.Atoi(max)
 
-			//listen for ICMP response
-			length, srcIP, icmp_err := icmp_conn.ReadFrom(msg)
-			if icmp_err != nil {
-				log.Println(icmp_err)
-				continue
-			}
-			if length == 0 && srcIP == nil {
-				fmt.Println("Open|Filtered")
-			} else {
-				test := srcIP.String()
-				if test == *IPPtr {
-					fmt.Println("We got a packet")
-					fmt.Println(string(msg))
+			var timeout, icmp_rtrn_3, icmp_rtrn_other bool = false, false, false
+			_, _ = icmp_rtrn_3, icmp_rtrn_other
+			for j := 0; j < int_max; j++ {
+				_, _ = conn.Write(buffer) //send buffered data
+
+				//ICMP_test_rtrn_3(ip_str) //TESTING FUNCTION
+				length, srcIP, icmp_err := icmp_conn.ReadFrom(msg)
+				_, _ = length, srcIP
+				if icmp_err != nil {
+					err_string := icmp_err.Error()
+					if strings.Contains(err_string, "i/o timeout") {
+						timeout = true
+					}
+					//need to swap in icmp collect here so it can run multiple times
+					//add some vars to store and use them instead to check later
+
+					//need to generate icmp echo reply packets for testing ??
 				}
-
 			}
+			if timeout {
+				fmt.Println("port:", currPort, " Open | Filtered")
+			} else if icmp_rtrn_3 {
+				fmt.Println("port:", currPort, " Open")
+			} else if icmp_rtrn_other {
+				fmt.Println("port:", currPort, " Filtered")
+			}
+			//listen for ICMP response
+			//no icmp packet means no reponse and port
 
+			/*
+				if icmp_err != nil {
+					err_string := icmp_err.Error()
+					if strings.Contains(err_string, "i/o timeout") {
+						fmt.Println("Open |  Filtered")
+					}
+				}
+				if srcIP != nil {
+					test := srcIP.String()
+					if test == *IPPtr {
+						fmt.Println("Port: ", currPort, " Status: Closed")
+					} else if length == 0 && srcIP == nil {
+						fmt.Println("port:", currPort, " Status: Open")
+					} else {
+						fmt.Println("where did this come from")
+					}
+				}*/
 			conn.Close()
 			icmp_conn.Close()
 		}
@@ -184,4 +222,37 @@ func Scanner(IP string, port int, proto string, timeout time.Duration) bool {
 	}
 	conn.Close() //closes connecton
 	return true
+}
+
+func handle_err(x error) {
+	if x != nil {
+		log.Fatal(x)
+		os.Exit(3)
+	}
+}
+
+func ICMP_test_rtrn_3(addr string) {
+	//tests returning ICMP error 3'
+	conn, _ := icmp.ListenPacket("ip4:icmp", "127.0.0.1")
+	defer conn.Close()
+	target_ip, err := net.ResolveIPAddr("ip4", addr)
+	handle_err(err)
+	m := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 3,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1, //<< uint(seq), // TODO
+			Data: []byte(""),
+		},
+	}
+	bytes, err := m.Marshal(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = conn.WriteTo(bytes, target_ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func ICMP_test_rtrn_other() {
+	// tests returning ICMP error of any type other than 3
 }
